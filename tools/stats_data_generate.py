fce59f710a3e6d14ff4855671afe3a5334e58e2c
Zach Lasiuk <zach.lasiuk@arm.com>
2023-06-12 12:17:46 -0500

    updated tooling script

--- tools/stats_data_generate.py
+++ tools/stats_data_generate.py
@@ -402,7 +402,7 @@ def main():
         # Check if date already a key in there
         exists=False
         for dic in existing_weekly_dic:
-            if date_today == dic.date:
+            if date_today == dic["date"]:
                 print('date today included, don"t save')
                 exists=True
                 break
